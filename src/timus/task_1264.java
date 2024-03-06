54fc573849335f277afebeb76983e424eeebfc72
Deout <tolya0906@gmail.com>
2024-03-06 19:14:46 +0500

    task_1264

---
+++ src/timus/task_1264.java
@@ -0,0 +1,13 @@
+package timus;
+
+import java.util.Scanner;
+public class task_1264 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+        System.out.println(N * (M + 1));
+    }
+}
